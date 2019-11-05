ad44687b854713916ce3e32a68a22f7f43fcf97b
BlewBlueSky <teagod@qq.com>
2019-11-05 16:51:38 +0800

    git push -u origin master

---
+++ BlewBlueSky/StudyGo/day03/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jacobsa/go-serial/serial"
+)
+
+func main() {
+
+	// Set up options.
+	options := serial.OpenOptions{
+		PortName:        "COM7",
+		BaudRate:        19200,
+		DataBits:        8,
+		StopBits:        1,
+		MinimumReadSize: 4,
+	}
+
+	// Open the port.
+	port, err := serial.Open(options)
+	if err != nil {
+		log.Fatalf("serial.Open: %v", err)
+	}
+
+	// Make sure to close it later.
+	defer port.Close()
+
+	// Write 4 bytes to the port.
+	b := []byte{0x00, 0x01, 0x02, 0x03}
+	n, err := port.Write(b)
+	if err != nil {
+		log.Fatalf("port.Write: %v", err)
+	}
+
+	fmt.Println("Wrote", n, "bytes.")
+
+	return
+
+}
